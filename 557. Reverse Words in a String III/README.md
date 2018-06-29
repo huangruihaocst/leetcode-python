cec383e0a5298a1cbf8b288fceb32c162b1f67c3
Ruihao Huang <huangruihaocst@126.com>
2018-06-29 19:02:14 +0800

    557. Reverse Words in a String III.

---
+++ 557. Reverse Words in a String III/README.md
@@ -0,0 +1,5 @@
+# 557. Reverse Words in a String III
+
+## Solution
+
+这题就注意到不要在`join`之前把`map`转成`list`了，因为`map`本身就是`iterable`。
---
+++ 557. Reverse Words in a String III/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ' '.join(map(lambda s: s[::-1], s.split(' ')))
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseWords("Let's take LeetCode contest"))
