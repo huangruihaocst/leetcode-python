89cab09fb1d058bcf5ab7a9a221a598bd71be4a2
Ruihao Huang <huangruihaocst@126.com>
2018-06-30 20:50:32 +0800

    136. Single Number.

---
+++ 136. Single Number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = dict()
+        for i in nums:
+            d[i] = d[i] + 1 if i in d else 1
+        for i in d:
+            if d[i] == 1:
+                return i
+                
+if __name__ == '__main__':
+    s = Solution()
+    print(s.singleNumber([4,1,2,1,2]))
