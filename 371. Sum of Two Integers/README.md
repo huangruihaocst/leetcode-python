377c884e3992afa95c6c5918b1372e75213ef230
Ruihao Huang <huangruihaocst@126.com>
2018-07-19 14:32:49 +0800

    371. Sum of Two Integers.

---
+++ 371. Sum of Two Integers/README.md
@@ -0,0 +1,5 @@
+# 371. Sum of Two Integers
+
+## Solution
+
+最快的方法过于奇葩。不过按位运算的方法很重要，要学会。可以参考[[编程题]不用加减乘除做加法](https://www.nowcoder.com/questionTerminal/59ac416b4b944300b617d4f7f111b215)和[不用＋、－、×、÷对两个数求和](https://blog.csdn.net/zhang_shuai12/article/details/46462175)。
---
+++ 371. Sum of Two Integers/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        if b == 0:
+        	return a
+        tmp = a ^ b
+        b = (a & b) << 1
+        a = tmp
+        return self.getSum(a, b)
+
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.getSum(5, 7))
