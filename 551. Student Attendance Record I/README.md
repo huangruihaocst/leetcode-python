0188e4584bb4835224e3b2544a728d78eb033e68
Ruihao Huang <huangruihaocst@126.com>
2018-07-25 13:29:23 +0800

    551. Student Attendance Record I.

---
+++ 551. Student Attendance Record I/README.md
@@ -0,0 +1,5 @@
+# 551. Student Attendance Record I
+
+## Solution
+
+仔细阅读题。
---
+++ 551. Student Attendance Record I/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return not (s.count('A') > 1 or 'LLL' in s)
