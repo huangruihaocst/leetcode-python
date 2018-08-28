a893386b41ef1e4736a9562a1c99040d681a3978
Ruihao Huang <huangruihaocst@126.com>
2018-08-27 17:23:44 -0700

    747. Largest Number At Least Twice of Others.

--- 747. Largest Number At Least Twice of Others/solution.py
+++ 747. Largest Number At Least Twice of Others/solution.py
@@ -14,9 +14,19 @@ class Solution:
         # return nums.index(m)
 
         # Solution 2:
-        # TODO
+        big1, big2 = (-1, float('-inf')), (-1, float('-inf'))
+        for i, v in enumerate(nums):
+            if v > big1[1]:
+                big2 = big1
+                big1 = i, v
+            elif v > big2[1]:
+                big2 = i, v
+        if big1[1] >= big2[1] * 2:
+            return big1[0]
+        else:
+            return -1
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.dominantIndex([1, 2, 3, 4]))
+    print(s.dominantIndex([3, 6, 1, 0]))
