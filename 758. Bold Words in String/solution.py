f8132e92415a6f27d3dd18fadf81a2239f928183
Ruihao Huang <huangruihaocst@126.com>
2018-09-18 20:17:50 -0700

    758. Bold Words in String.

--- 758. Bold Words in String/solution.py
+++ 758. Bold Words in String/solution.py
@@ -5,3 +5,33 @@ class Solution:
         :type S: str
         :rtype: str
         """
+        import re
+        marked = [False] * len(S)
+        for word in words:
+            start = [m.start() for m in re.finditer('(?=' + word + ')', S)]
+            for s in start:
+                for i in range(s, s + len(word)):
+                    marked[i] = True
+        res = ''
+        bold = False
+        for i, c in enumerate(S):
+            if bold:
+                if not marked[i]:
+                    res += '</b>' + c
+                    bold = False
+                else:
+                    res += c
+            else:
+                if marked[i]:
+                    res += '<b>' + c
+                    bold = True
+                else:
+                    res += c
+        if bold:
+            res += '</b>'
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.boldWords(["ab", "bc"], 'aabcd'))
